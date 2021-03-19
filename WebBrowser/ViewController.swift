a1235bc6402953faeee847e9ce1c0b68b9beef91
sdash-usfca <sdash@dons.usfca.edu>
2021-03-19 01:32:50 -0400

    Initial Commit

---
+++ WebBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebBrowser
+//
+//  Created by Sukirti Dash on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
